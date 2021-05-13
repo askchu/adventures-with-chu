6c9024cb18d00398d20be2b3a812bc780e368bf8
Alex Chu <alex.chu16@hotmail.com>
2021-05-13 14:43:06 -0400

    fixed profile gallery link to only show from blog posts instead of firestore

--- src/component/Profile/ProfileGallery/ProfileGallery.js
+++ src/component/Profile/ProfileGallery/ProfileGallery.js
@@ -1,19 +1,73 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import instance from '../../../axios-orders';
+import { useAuth } from '../../Authentication/AuthContext/AuthContext';
 import ImageGrid from '../../Blogging/ImageGrid/ImageGrid';
 import Modal from '../../Blogging/ImageGrid/Modal/Modal';
 import Profile from '../Profile';
 
 export default function ProfileGallery() {
+    const { currentUser } = useAuth();
+    const [blogData, setBlogData] = useState([]);
     const [selectedImg, setSelectedImg] = useState(null);
+
+    useEffect(async () => {
+        await instance.get(`users/${currentUser.uid}/blogs.json`)
+            .then(response => {
+                console.log(response.data)
+                let data = [];
+                for (let key in response.data) {
+                    data.push({
+                        ...response.data[key],
+                        id: key
+                    })
+                }
+                setBlogData(data);
+            })
+            .catch(err => console.log(err));
+    }, [])
+
+    let image = [];
+    if (blogData) {
+        if (blogData.length > 0) {
+            blogData.map(doc => {
+                console.log(doc.images);
+                let imageValues = Object.values(doc.images);
+                console.log(imageValues);
+                imageValues.map(img => {
+                    image.push({
+                        id: img.id,
+                        url: img.imageUrl
+                    });
+                })
+                console.log(image);
+            })
+            // image = blogData.map(doc => (
+            //     <div className='img-wrap' key={doc.id}>
+
+            //     </div>
+            // ))
+        }
+    }
+    console.log(selectedImg);
+
+    const imgClicked = (img) => {
+        setSelectedImg(img);
+    }
 
     return (
         <div className='container'>
             <Profile />
             <div className='profile-content'>
-                <ImageGrid setSelectedImg={setSelectedImg} />
-                {selectedImg &&
-                    <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />}
+                <div className='img-grid'>
+                    {image.map(doc => (
+                        <div className='img-wrap' key={doc.id}>
+                            <img src={doc.url} onClick={() => imgClicked(doc.url)} />
+                        </div>
+                    ))}
+                    {selectedImg &&
+                        <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />}
+                </div>
             </div>
-        </div>
+        </div >
     )
 }
